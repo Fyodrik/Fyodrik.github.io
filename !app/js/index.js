a8c713ce405aa76a8c1385b023dceb25a792c21c
Fyodrik <fyodrik@yandex.ru>
2015-03-25 00:52:13 +0500

    First commit

---
+++ !app/js/index.js
@@ -0,0 +1,37 @@
+function update() {
+	if(window.innerHeight-104<=480)
+		$('#content').css({
+			'height':'480px'
+		});
+	else
+		$('#content').css({
+			'height':window.innerHeight-119+'px'
+		});
+}
+update();
+window.addEventListener("resize", update);
+// <!-- Yandex.Metrika counter -->
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter28359591 = new Ya.Metrika({id:28359591,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    trackHash:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+// <!-- /Yandex.Metrika counter -->
