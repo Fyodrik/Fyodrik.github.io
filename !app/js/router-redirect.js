a9e1f78254985b939c9af844f2a2f3fcbb6f2470
Fyodrik <fyodrik@yandex.ru>
2015-03-25 11:49:26 +0500

    First commit

--- !app/js/router-redirect.js
+++ !app/js/router-redirect.js
@@ -1,5 +1,7 @@
 var sCurLoc = location.toString();
 
+document.getElementById('err404Title').innerHTML = "Redirecting..."
+
 function locRedirect(oRouters){
 	for(var i = 0; i<oRouters.List.length; i++)
 		if(sCurLoc==oRouters.List[i].FullLink){
