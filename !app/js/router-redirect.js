e7686c3b9086df8731f0ffb3d47a69c294daefc1
Fyodrik <fyodrik@yandex.ru>
2015-03-25 12:43:00 +0500

    First commit

--- !app/js/router-redirect.js
+++ !app/js/router-redirect.js
@@ -1,8 +1,8 @@
-(function(){
+function(){
 	for(var i = 0; i<oRouters.List.length; i++)
 		if(sCurLoc==oRouters.List[i].FullLink){
 			document.getElementById('err404Title').innerHTML = "Redirecting...";
 			var a = oRouters.List[i].FromLoc+"#"+oRouters.List[i].UrlHash;
 			location = a;
 		}
-})();
+}();
