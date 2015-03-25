6389b3b0d09ecc9f89d7112e784af452cc9bac89
Fyodrik <fyodrik@yandex.ru>
2015-03-25 11:16:55 +0500

    First commit

--- !app/js/router-redirect.js
+++ !app/js/router-redirect.js
@@ -1,31 +1,15 @@
-var oRouters, sCurLoc = location.toString();
+var sCurLoc = location.toString();
 
-function getJSON(oRouters){
-	$.getJSON("!app/js/routersDB.json",function(data){
-		oRouters = data;
-	})
-}
-
-function locRedirect(){
-	alert(oRouters);
-	alert(oRouters.List);
-	alert(oRouters.List.length);
-	alert(oRouters.List[0]);
+function locRedirect(oRouters){
 	for(var i = 0; i<oRouters.List.length; i++)
-			alert("REPEAT");
-			alert(oRouters);
-			alert(oRouters.List);
-			alert(oRouters.List.length);
-			alert(oRouters.List[0]);
-			alert("iF");
-			alert(sCurLoc==oRouters.List[i].FullLink);
-			if(sCurLoc==oRouters.List[i].FullLink){
-				alert("YE!");
-				alert(oRouters.List[i].FullLink);
-				// location = oRouters.List[i].FromLoc+"#"+oRouters.List[i].UrlHash;
-			}
+		alert(sCurLoc==oRouters.List[i].FullLink);
+		if(sCurLoc==oRouters.List[i].FullLink){
+			alert("YE!");
+			alert(oRouters.List[i].FullLink);
+			// location.href = oRouters.List[i].FromLoc+"#"+oRouters.List[i].UrlHash;
+		}
 }
 
-getJSON(oRouters);
-alert(sCurLoc==oRouters.List[i].FullLink);
-locRedirect();
+$.getJSON("!app/js/routersDB.json",function(data){
+		locRedirect(data)
+	})
