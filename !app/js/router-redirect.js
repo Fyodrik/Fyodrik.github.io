0a09a4304ee7fdb2b6b44c24c3fd26c5bfde861e
Fyodrik <fyodrik@yandex.ru>
2015-03-25 11:53:12 +0500

    First commit

--- !app/js/router-redirect.js
+++ !app/js/router-redirect.js
@@ -6,6 +6,7 @@ function locRedirect(oRouters){
 	for(var i = 0; i<oRouters.List.length; i++)
 		if(sCurLoc==oRouters.List[i].FullLink){
 			var a = oRouters.List[i].FromLoc+"#"+oRouters.List[i].UrlHash;
+			alert(a);
 			location = a;
 		}
 }
