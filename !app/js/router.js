e44070ad86aef0311cac3138db809479e41e332c
Dmitry Fyodorov <fyodrik@yandex.ru>
2015-12-25 06:30:06 +0500

    Start

--- !app/js/router.js
+++ !app/js/router.js
@@ -1,6 +1,10 @@
 ;var oRouters, sCurLoc = location.toString(), hostdomen = "https://fyodrik.github.io/";
 
-
+(function(){
+	$.getJSON("!app/js/routersDB.json",function(data){
+		oRouters = data;
+	});
+})();
 
 //Обновление состояния
 
@@ -13,26 +17,20 @@ var updateState = function(state){
 			return;
 	else sUrlHash = state.page;
 	do{
-		(function(){
-			$.getJSON("!app/js/routersDB.json",function(data){
-				oRouters = data;
-			});
-		})();
 		if(oRouters)
 			for(var i = 0; i<oRouters.List.length; i++){
 				if(location.pathname.slice(1)==oRouters.List[i].UrlHash){
-					if(sCurLoc!=oRouters.List[i].FromLoc){
+					/*if(sCurLoc!=oRouters.List[i].FromLoc){
 						// exit_n();
 					}
 					else{}
-						// exit();
+						// exit();*/
 					err404=false;
 					document.querySelector('title').innerHTML = oRouters.List[i].Title;
 					$('#content').load(oRouters.List[i].ContentLink);
 					return;
 				}
 			}
-		else{}
 	}while(!oRouters)
 	if(!err404)
 		location = a = hostdomen+location.pathname.slice(1);
