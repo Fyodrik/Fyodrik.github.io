76d5276ed9f1719e77f831340c243503edae64af
Dmitry Fyodorov <fyodrik@yandex.ru>
2015-12-30 21:17:53 +0500

    Start

--- !app/js/index.js
+++ !app/js/index.js
@@ -1,12 +1,12 @@
 //Adaptive
 function adaptiveContentHeight(){
-    if(window.innerHeight-51<=480)
+    if(window.innerHeight-52<=480)
         $('#content').css({
             'height':'480px'
         });
     else
         $('#content').css({
-            'height':window.innerHeight-52+'px'
+            'height':window.innerHeight-53+'px'
         });
     $('#content').css({
             'opacity':1
