c5f32be623e08b4edc8042c7917d9a3087d84cc6
Dmitry Fyodorov <fyodrik@yandex.ru>
2015-12-30 17:55:12 +0500

    Start

--- !app/js/nav-filter.js
+++
@@ -1,8 +0,0 @@
-(A = function(){
-    $('#nav-container').css({
-        'width': parseInt(window.innerWidth/398)*398+'px'
-    });
-})();
-
-window.addEventListener("load", A);
-window.addEventListener("resize", A);
