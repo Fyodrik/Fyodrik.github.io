9ea5d49c0bd92a729dd3f06a9adba6631f71c2df
Dmitry Fyodorov <fyodrik@yandex.ru>
2016-01-04 15:08:44 +0500

    Start

--- !app/js/navContainer.js
+++ !app/js/navContainer.js
@@ -43,3 +43,6 @@ var adaptiveNavContainer = function(){
     });
     console.log("ADAPTIVING nav-container");
 }
+adaptiveNavContainer();
+window.addEventListener("load", adaptiveNavContainer);
+window.addEventListener("resize", adaptiveNavContainer);
